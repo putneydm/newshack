a2c4af56c201e6593cc7ea497b35c8b250755651
putneydm <david@davidputney.com>
2014-06-08 09:59:32 -0400

    adds code that hides and shows the holder box.

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,25 @@ $(document) .ready (function()
 	console.log('works');
 	
 
+	$('.bingo_grid li') .click (function ()
+	{	
+	$('#holder') .fadeOut (100);
+	var width = $('.bingo_grid') .width ();	
+	if (width < 550)
+		{
+		var contents = $(this) .clone ();
+		$('#holder') .fadeIn (300);
+		$('#holder li') .remove ();
+		$('#holder') .append (contents);
+		}
+	});
+	
+	
+	$('.close_box') .click (function ()
+		{	
+		$('#holder') .fadeOut (100);
+		});
+	
 	
 	
 	
